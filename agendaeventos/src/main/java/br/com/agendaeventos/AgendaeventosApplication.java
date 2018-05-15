59b86088755398a6c38462893ca288d9a9deded6
marcospmoliveira <marcospaulomeiradeoliveira@gmail.com>
2018-05-15 15:40:15 -0300

    first commit

---
+++ agendaeventos/src/main/java/br/com/agendaeventos/AgendaeventosApplication.java
@@ -0,0 +1,12 @@
+package br.com.agendaeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaeventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaeventosApplication.class, args);
+	}
+}
